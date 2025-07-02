2b14c51da985371aef012bded81589d129efcb5e
KennyWouters <kenny.wouters2@gmail.com>
2025-07-02 23:54:46 +0200

    WIP Designing the shop

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,26 +1,17 @@
-import {Component, inject, OnInit} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {Header} from './layout/header/header';
-import {HttpClient} from '@angular/common/http';
+import {Component } from '@angular/core';
+
+import {HeaderComponent} from './layout/header/header';
+import {ShopComponent} from './features/shop/shop';
+
+
 
 @Component({
   selector: 'app-root',
   standalone : true,
-  imports: [RouterOutlet, Header],
+  imports: [ HeaderComponent, ShopComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit {
-  baseUrl = 'https://localhost:7277/api/';
-  private http= inject(HttpClient);
+export class AppComponent {
   title = 'Skinet';
-  products: any = [];
-
-  ngOnInit() {
-    this.http.get<any>(this.baseUrl+ 'products').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => console.log('complete'),
-    })
-  }
 }
