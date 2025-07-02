b296876259261dc30226c5d653b50eacd73db2d2
KennyWouters <kenny.wouters2@gmail.com>
2025-07-02 15:40:43 +0200

    created product specParams

--- Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
+++ Infrastructure/obj/Debug/net9.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1408c93f0ddd2c3c7638e269c5ea1a7581909d77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ab95b35b1a8125a82923eafc10ca599e6f8708")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
