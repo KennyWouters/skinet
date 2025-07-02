38ab95b35b1a8125a82923eafc10ca599e6f8708
KennyWouters <kenny.wouters2@gmail.com>
2025-07-02 15:16:37 +0200

    End of Section 4 Specification Pattern

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be039f961bc2d94e0546c124480f5a0cb350ffe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1408c93f0ddd2c3c7638e269c5ea1a7581909d77")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
